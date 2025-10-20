50051fa0b70e50a9c5f19eb762ce7ee7e92bf75c
GitHub Action <action@github.com>
2025-10-20 07:18:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/scccy/easycodeplus/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/scccy/easycodeplus/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.scccy.easycodeplus.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
